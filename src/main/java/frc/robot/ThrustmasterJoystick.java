fd88234dee66d3796dfbe8926a634459d26e1207
Christopher Burie <102393796+ChristopherBurie@users.noreply.github.com>
2024-02-05 14:33:11 -0600

    Updated

--- src/main/java/frc/robot/ThrustmasterJoystick.java
+++ src/main/java/frc/robot/ThrustmasterJoystick.java
@@ -5,41 +5,6 @@
 package frc.robot;
 
 /** Add your docs here. */
-public class ThrustmasterJoystick {
-    public static final int button3 = 3;
-    public static final int button4 = 4;
-    public static final int button5 = 5;
-    public static final int button6 = 6;
-    public static final int button7 = 7;
-    public static final int button8 = 8;
-    public static final int button9 = 9;
-    public static final int button10 = 10;
-    public static final int button11 = 11;
-    public static final int button12 = 12;
-
-    public static final int trigger = 1;
-    public static final int thumbButton = 2;
-
-    public static final int xAxis = 0;
-    public static final int yAxis = 1;
-    public static final int rotationAxis = 2;
-    public static final int throttleAxis = 3;
-
-    public static enum POVState
-    {
-        CENTER(-1),
-        LEFT(270),
-        RIGHT(90),
-        UP(0),
-        DOWN(180);
-
-        private final int value;
-
-        POVState(final int value)
-        {
-            this.value = value;
-        }
-
-        public int value() {return value;};
-    }
+public class Thrustmaster implements IJoystick {
+    
 }
