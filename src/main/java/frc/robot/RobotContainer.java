099f5f77c135af49af387c024b01792b1705d9b1
Christopher Burie <102393796+ChristopherBurie@users.noreply.github.com>
2024-02-19 16:35:02 -0600

    Updated

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,14 +1,11 @@
 package frc.robot;
 
-import org.ietf.jgss.Oid;
-
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.OIConstants;
 import frc.robot.autos.*;
@@ -85,7 +82,7 @@ public class RobotContainer {
      */
     private void configureButtonBindings() {
         /* Driver Buttons */
-        OIConstants.homeButton.whileTrue(
+        OIConstants.homeButton.onTrue(
             new SequentialCommandGroup(
                 s_Pivot.setPivotAngle(Rotation2d.fromDegrees(7)),
                 s_Elevator.setHeight(Units.inchesToMeters(12.9)),
